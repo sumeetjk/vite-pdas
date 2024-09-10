f61e1950bd01c9068024d8521d8c2633f9e2c055
sumeet kumawat <sumeetjkumawat@gmail.com>
2024-09-10 22:33:10 +0530

    move images to src and use props system

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
@@ -6,9 +9,9 @@ function App() {
       <h1>Personal Digital Assistants</h1>
 
       <hr />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,5 +1,19 @@
-function ProfileCard() {
-  return <h1>Profile Card</h1>;
+// 3rd approach Argument destructuring.
+function ProfileCard({ title, handle, image }) {
+  // 1st approach
+  //   const title = props.title;
+  //   const handle = props.handle;
+
+  // 2nd approach destructuring
+  //const { title, handle } = props;
+
+  return (
+    <>
+      <img src={image} alt="pda logo" />
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </>
+  );
 }
 
 export default ProfileCard;
